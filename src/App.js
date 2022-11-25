8a628645238d97c1a12569db2169d0833e24a00a
evaaldo <evaaldormj@gmail.com>
2022-11-25 15:38:06 -0300

    Criação do primeiro componente em React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Cabecalho from './Componentes/Cabecalho/Cabecalho';
+
+function App() {
+  return (
+    <div className="App">
+      <Cabecalho></Cabecalho>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Cabecalho/Cabecalho.js
@@ -0,0 +1,9 @@
+function Cabecalho() {
+    return (
+        <div>
+            <h1>Olá, mundo!</h1>
+        </div>
+    )
+}
+
+export default Cabecalho
