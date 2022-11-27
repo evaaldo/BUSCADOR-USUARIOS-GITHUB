4c0d4ff58f816eaffff047ba4af533620ad9c033
evaaldo <evaaldormj@gmail.com>
2022-11-27 12:57:56 -0300

    Consumindo API do github user por meio do axios

--- src/Componentes/ContainerCima/ContainerCima.js
+++ src/Componentes/ContainerCima/ContainerCima.js
@@ -2,7 +2,25 @@ import './ContainerCima.css'
 import { BsSun } from 'react-icons/bs'
 import { BsMoon } from 'react-icons/bs'
 
+import axios from 'axios'
+import { useState } from 'react'
+
 function ContainerCima() {
+
+    const [search, setSearch] = useState('')
+
+    const GITHUBResponse = {
+        name: String,
+        bio: String,
+        avatar_url: String
+    }
+
+    const handleSearch = () => {
+
+        axios.get(`https://api.github.com/users/${search}`)
+            .then(res => console.log(res.data))
+    }
+
     return (
         <div className="containerTopo">
             <div className='containerTopo__titulo'>
@@ -14,8 +32,9 @@ function ContainerCima() {
                 </label>
             </div>
             <div className='containerTopo__input'>
-                <input className='containerTopo__text' type='text' placeholder='Procute um usuário do GitHub...'></input>
-                <button className='containerTopo__botao'>Pesquisar</button>
+                <input className='containerTopo__text' type='text' placeholder='Procure um usuário do GitHub...' 
+                onChange={(e) => setSearch(e.target.value)}></input>
+                <button className='containerTopo__botao' onClick={handleSearch}>Pesquisar</button>
             </div>
         </div>
     )
