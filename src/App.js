acb070f677e2035618c608b247faba2ce6553c3d
evaaldo <evaaldormj@gmail.com>
2022-11-29 11:06:43 -0300

    Início da criação da lógica de trocar o tema da página

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { createContext, useState } from 'react';
 import './App.css';
 import Container from './Componentes/Container/Container';
 
+export const ThemeContext = createContext('light')
+
 function App() {
+
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === 'light' ? 'dark' : 'light'))
+  }
+
   return (
-    <div className="App">
-      <div>
-        <Container></Container>
-      </div>      
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <div>
+          <Container></Container>
+        </div>      
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/Componentes/Container/Container.js
+++ src/Componentes/Container/Container.js
@@ -1,5 +1,6 @@
 import './ContainerCima.css'
 import './ContainerBaixo.css'
+import { createContext } from 'react';
 import { BsSun } from 'react-icons/bs'
 import { BsMoon } from 'react-icons/bs'
 import { BsGeoAlt } from 'react-icons/bs'
@@ -8,7 +9,15 @@ import { BsLink } from 'react-icons/bs'
 import axios from 'axios'
 import { useState } from 'react'
 
+export const ThemeContext = createContext('light')
+
 function Container() {
+
+    const [theme, setTheme] = useState('light')
+
+    const toggleTheme = () => {
+        setTheme((curr) => (curr === 'light' ? 'dark' : 'light'))
+      }
 
     const [search, setSearch] = useState('')
     const [name, setName] = useState('Usuário')
@@ -36,56 +45,59 @@ function Container() {
     }
 
     return (
-        <div className='container'>
-            <div className="containerTopo">
-                <div className='containerTopo__titulo'>
-                    <h3>devFinder</h3>
-                    <input type='checkbox' id='change-theme'></input>
-                    <label for='change-theme'>
-                        <BsSun className='sol'/>
-                        <BsMoon className='lua'/>
-                    </label>
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            <div className='container' id={theme}>
+                <div className="containerTopo">
+                    <div className='containerTopo__titulo'>
+                        <h3>devFinder</h3>
+                        <input type='checkbox' id='change-theme' onChange={toggleTheme} checked={theme === 'light'}></input>
+                        <label for='change-theme'>
+                            <BsSun className='sol'/>
+                            <BsMoon className='lua'/>
+                        </label>
+                    </div>
+                    <div className='containerTopo__input'>
+                        <input className='containerTopo__text' type='text' placeholder='Procure um usuário do GitHub...' 
+                        onChange={(e) => setSearch(e.target.value)}></input>
+                        <button className='containerTopo__botao' onClick={handleSearch}>Pesquisar</button>
+                    </div>
                 </div>
-                <div className='containerTopo__input'>
-                    <input className='containerTopo__text' type='text' placeholder='Procure um usuário do GitHub...' 
-                    onChange={(e) => setSearch(e.target.value)}></input>
-                    <button className='containerTopo__botao' onClick={handleSearch}>Pesquisar</button>
+                <div className="containerBaixo">
+                    <div className='containerBaixo__ícone'>
+                        <img className='containerBaixo__foto' src={avatar_url}></img>
+                    </div>
+                    <div className='containerBaixo__infos'>
+                        <div className='info-1'>
+                            <h2 className='containerBaixo__nome'>{name}</h2>
+                        </div>
+                        <div className='info-2'>
+                            <p className='containerBaixo__bio'>{bio}</p>
+                        </div>
+                        <div className='info-3'>
+                            <div className='info'>
+                                <p className='info__name'>Followers</p>
+                                <p className='info__value'>{followers}</p>
+                            </div>
+                            <div className='info'>
+                                <p className='info__name'>Following</p>
+                                <p className='info__value'>{following}</p>
+                            </div>
+                        </div>
+                        <div className='info-4'>
+                            <div className='local'>
+                                <BsGeoAlt size={20}/>
+                                <p className='info__value'>{location}</p>
+                            </div>
+                            <div className='link'>
+                                <BsLink size={20}/>
+                                <a href={blog} className='info__value'>{blog}</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="containerBaixo">
-                <div className='containerBaixo__ícone'>
-                    <img className='containerBaixo__foto' src={avatar_url}></img>
-                </div>
-                <div className='containerBaixo__infos'>
-                    <div className='info-1'>
-                        <h2 className='containerBaixo__nome'>{name}</h2>
-                    </div>
-                    <div className='info-2'>
-                        <p className='containerBaixo__bio'>{bio}</p>
-                    </div>
-                    <div className='info-3'>
-                        <div className='info'>
-                            <p className='info__name'>Followers</p>
-                            <p className='info__value'>{followers}</p>
-                        </div>
-                        <div className='info'>
-                            <p className='info__name'>Following</p>
-                            <p className='info__value'>{following}</p>
-                        </div>
-                    </div>
-                    <div className='info-4'>
-                        <div className='local'>
-                            <BsGeoAlt size={20}/>
-                            <p className='info__value'>{location}</p>
-                        </div>
-                        <div className='link'>
-                            <BsLink size={20}/>
-                            <a href={blog} className='info__value'>{blog}</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </ThemeContext.Provider>
+        
     )
 }
 
